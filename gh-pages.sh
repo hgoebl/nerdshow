1193323c160bf169ec02d76d448708d9959e6c9d
Heinrich Goebl <hgoebl@goebl.com>
2013-06-16 19:15:43 +0200

    use new jsdom version (jsdom issue #436)

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,12 +4,12 @@ DEST=../nerdshow@gh-pages
 RT=rt
 
 # generate slides for gh-pages and copy to branch
-nerdshow-generate examples/about/presentation.json --nerdshow-folder $RT --no-socketio-enabled --no-zoom-enabled
+bin/nerdshow-generate examples/about/presentation.json --nerdshow-folder $RT --no-socketio-enabled --no-zoom-enabled
 
 cp -ru examples/about/* $DEST/
 
 # recreate slides for node.js demo
-nerdshow-generate examples/about/presentation.json
+bin/nerdshow-generate examples/about/presentation.json
 
 # copy / update runtime
 mkdir -p $DEST/$RT
